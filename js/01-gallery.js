0a21f70247bce78a3c32b5b37342dd528eab1792
jaycikey <138725941+jaycikey@users.noreply.github.com>
2023-11-12 15:11:35 -0500

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleyItem(item) {
+  // Створеня єлементу списку для картинки
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery_item");
+
+  // Створення линки для картинки
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  // Створення картинки
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.setAttribute("data-source", item.original);
+  galleryImage.alt = item.description;
+
+  // Огортаємо
+  galleryItem.appendChild(galleryLink);
+  galleryLink.appendChild(galleryImage);
+
+  return galleryItem;
+}
+
+// Переберання массиву
+const galleryItem = galleryItems.map((item) => createGalleyItem(item));
+
+// Розгортання(spread) та додавання в DOM
+gallery.append(...galleryItem);
+
+// Слухач на ul
+gallery.addEventListener("click", (event) => {
+  // Вимикання дефолтної події
+  event.preventDefault();
+
+  // Отримання лінки на велике зображення та опису
+  const largeImageLink = event.target.dataset.source;
+  const imgAlt = event.target.alt;
+
+  // Обробка кліка тільки по картинці
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // Відкриття модального вікна
+  const instance = basicLightbox.create(`
+  <img src="${largeImageLink}" alt = "${imgAlt}" />
+  `);
+
+  instance.show();
+
+  // Закриття модального вікна клавішею Esc
+  window.addEventListener("keydown", (event) => {
+    if(event.key === "Escape"){
+        instance.close();
+    }
+  });
+});
